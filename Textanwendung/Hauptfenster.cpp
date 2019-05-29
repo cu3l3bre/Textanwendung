5b38c43e3e29e62291417d34d6445a6c79882aea
cu3l3bre <cu3l3bre@googlemail.com>
2019-05-29 11:53:49 +0200

    added some buttons and textboxes

--- Textanwendung/Hauptfenster.cpp
+++ Textanwendung/Hauptfenster.cpp
@@ -1,2 +1,46 @@
 #include "Hauptfenster.h"
 
+System::Void Textanwendung::Hauptfenster::button_TextAnfuegen_Click(System::Object ^ sender, System::EventArgs ^ e)
+{
+	// Text in der Eingabe lesen
+	//String^ eingabeText = textBox_Eingabe->Text;
+
+	// diesen Text an die Textbox zur Ausgabe anfügen
+	//textBox_Ausgabe->Text += eingabeText;
+
+	// oder halt direkt nä
+	if (textBox_Eingabe->Text != "")
+	{
+		textBox_Ausgabe->Text += "- " + textBox_Eingabe->Text;
+		textBox_Ausgabe->Text += "\r\n";
+		textBox_Eingabe->Text = "";
+		label_ZeichenZaehlen->Text = "0";
+	}
+	else
+	{
+		// Do nothing
+	}
+	textBox_Eingabe->Focus();
+}
+
+
+System::Void Textanwendung::Hauptfenster::button_ZeichenZaehlen_Click(System::Object^  sender, System::EventArgs^  e)
+{
+	//int laengeEingabetext = textBox_Eingabe->TextLength;
+	//label_ZeichenZaehlen->Text = laengeEingabetext.ToString();
+
+	// oder als einzeiler
+	//label_ZeichenZaehlen->Text = (textBox_Eingabe->TextLength).ToString();
+	
+	// geht auch so
+	label_ZeichenZaehlen->Text = textBox_Eingabe->TextLength.ToString();
+
+	textBox_Eingabe->Focus();
+
+	// note
+	// die variable, wird automatisch in den Datentyp Int32 umgewandelt,
+	// der eine Methode bereitstellt, die das in einen String umwandelt
+	// also von int nach int32, die hat die Methode
+	//Int32^ test = 6;
+	// test->ToString();
+}
--- Textanwendung/Hauptfenster.h
+++ Textanwendung/Hauptfenster.h
@@ -34,6 +34,16 @@ namespace Textanwendung {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::TextBox^  textBox_Eingabe;
+	protected:
+	private: System::Windows::Forms::Label^  label_Eingabe;
+	private: System::Windows::Forms::TextBox^  textBox_Ausgabe;
+	private: System::Windows::Forms::Button^  button_TextAnfuegen;
+	private: System::Windows::Forms::Button^  button_ZeichenZaehlen;
+	private: System::Windows::Forms::Label^  label_ZeichenZaehlen;
+
+
+
 
 	private:
 		/// <summary>
@@ -48,19 +58,102 @@ namespace Textanwendung {
 		/// </summary>
 		void InitializeComponent(void)
 		{
+			System::ComponentModel::ComponentResourceManager^  resources = (gcnew System::ComponentModel::ComponentResourceManager(Hauptfenster::typeid));
+			this->textBox_Eingabe = (gcnew System::Windows::Forms::TextBox());
+			this->label_Eingabe = (gcnew System::Windows::Forms::Label());
+			this->textBox_Ausgabe = (gcnew System::Windows::Forms::TextBox());
+			this->button_TextAnfuegen = (gcnew System::Windows::Forms::Button());
+			this->button_ZeichenZaehlen = (gcnew System::Windows::Forms::Button());
+			this->label_ZeichenZaehlen = (gcnew System::Windows::Forms::Label());
 			this->SuspendLayout();
+			// 
+			// textBox_Eingabe
+			// 
+			this->textBox_Eingabe->Font = (gcnew System::Drawing::Font(L"Calibri", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox_Eingabe->Location = System::Drawing::Point(47, 72);
+			this->textBox_Eingabe->Name = L"textBox_Eingabe";
+			this->textBox_Eingabe->Size = System::Drawing::Size(285, 40);
+			this->textBox_Eingabe->TabIndex = 0;
+			// 
+			// label_Eingabe
+			// 
+			this->label_Eingabe->AutoSize = true;
+			this->label_Eingabe->Font = (gcnew System::Drawing::Font(L"Calibri", 14.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label_Eingabe->Location = System::Drawing::Point(43, 46);
+			this->label_Eingabe->Name = L"label_Eingabe";
+			this->label_Eingabe->Size = System::Drawing::Size(234, 23);
+			this->label_Eingabe->TabIndex = 1;
+			this->label_Eingabe->Text = L"Bitte geben Sie einen Text ein:";
+			// 
+			// textBox_Ausgabe
+			// 
+			this->textBox_Ausgabe->Font = (gcnew System::Drawing::Font(L"Calibri", 20.25F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->textBox_Ausgabe->Location = System::Drawing::Point(47, 164);
+			this->textBox_Ausgabe->Multiline = true;
+			this->textBox_Ausgabe->Name = L"textBox_Ausgabe";
+			this->textBox_Ausgabe->ScrollBars = System::Windows::Forms::ScrollBars::Both;
+			this->textBox_Ausgabe->Size = System::Drawing::Size(285, 295);
+			this->textBox_Ausgabe->TabIndex = 2;
+			this->textBox_Ausgabe->WordWrap = false;
+			// 
+			// button_TextAnfuegen
+			// 
+			this->button_TextAnfuegen->Location = System::Drawing::Point(47, 118);
+			this->button_TextAnfuegen->Name = L"button_TextAnfuegen";
+			this->button_TextAnfuegen->Size = System::Drawing::Size(285, 40);
+			this->button_TextAnfuegen->TabIndex = 3;
+			this->button_TextAnfuegen->Text = L"Text anfuegen";
+			this->button_TextAnfuegen->UseVisualStyleBackColor = true;
+			this->button_TextAnfuegen->Click += gcnew System::EventHandler(this, &Hauptfenster::button_TextAnfuegen_Click);
+			// 
+			// button_ZeichenZaehlen
+			// 
+			this->button_ZeichenZaehlen->Location = System::Drawing::Point(338, 72);
+			this->button_ZeichenZaehlen->Name = L"button_ZeichenZaehlen";
+			this->button_ZeichenZaehlen->Size = System::Drawing::Size(285, 40);
+			this->button_ZeichenZaehlen->TabIndex = 4;
+			this->button_ZeichenZaehlen->Text = L"---> Zeichen zaehlen";
+			this->button_ZeichenZaehlen->UseVisualStyleBackColor = true;
+			this->button_ZeichenZaehlen->Click += gcnew System::EventHandler(this, &Hauptfenster::button_ZeichenZaehlen_Click);
+			// 
+			// label_ZeichenZaehlen
+			// 
+			this->label_ZeichenZaehlen->AutoSize = true;
+			this->label_ZeichenZaehlen->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
+			this->label_ZeichenZaehlen->Font = (gcnew System::Drawing::Font(L"Calibri", 21.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label_ZeichenZaehlen->Location = System::Drawing::Point(638, 73);
+			this->label_ZeichenZaehlen->Name = L"label_ZeichenZaehlen";
+			this->label_ZeichenZaehlen->Size = System::Drawing::Size(32, 38);
+			this->label_ZeichenZaehlen->TabIndex = 5;
+			this->label_ZeichenZaehlen->Text = L"0";
 			// 
 			// Hauptfenster
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(584, 508);
+			this->ClientSize = System::Drawing::Size(952, 508);
+			this->Controls->Add(this->label_ZeichenZaehlen);
+			this->Controls->Add(this->button_ZeichenZaehlen);
+			this->Controls->Add(this->button_TextAnfuegen);
+			this->Controls->Add(this->textBox_Ausgabe);
+			this->Controls->Add(this->label_Eingabe);
+			this->Controls->Add(this->textBox_Eingabe);
+			this->Icon = (cli::safe_cast<System::Drawing::Icon^>(resources->GetObject(L"$this.Icon")));
 			this->Name = L"Hauptfenster";
 			this->StartPosition = System::Windows::Forms::FormStartPosition::CenterScreen;
-			this->Text = L"Hauptfenster";
+			this->Text = L"Textanwendung";
 			this->ResumeLayout(false);
+			this->PerformLayout();
 
 		}
 #pragma endregion
-	};
+	private: System::Void button_TextAnfuegen_Click(System::Object^  sender, System::EventArgs^  e);
+
+
+private: System::Void button_ZeichenZaehlen_Click(System::Object^  sender, System::EventArgs^  e);
+};
 }
